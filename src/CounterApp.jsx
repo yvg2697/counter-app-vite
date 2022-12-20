45f85b4095c7388a82a512f82ccc5c352f26ba22
Yasiel Valdes <yasiel.valdes.azcuba.cu>
2022-12-20 11:49:35 -0500

    finish counter

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,21 +1,23 @@
 import { PropTypes } from "prop-types";
+import { useState } from "react";
 
 export const CounterApp = ({ value }) => {
     
-    const handleAdd = () => {
-        console.log('+1');
-        value = 1000;
-        console.log(value);
-    }
+  const [counter, setCounter] = useState(value);  
+
+  const handleAdd = () => { setCounter(counter + 1) };
+
+  const handleSubstract = () => { setCounter(counter - 1) };
+
+  const handleReset = () => { setCounter(value) };
 
   return (
     <>
           <h1> CounterApp </h1>
-          <h2> {value} </h2>
-          <button onClick= {handleAdd}>
-              +1
-          </button>
-
+          <h2> {counter} </h2>
+          <button onClick= {handleAdd}> +1 </button>
+          <button onClick= {handleSubstract}> -1 </button>
+          <button onClick= {handleReset}> Reset </button>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import { CounterApp } from "./CounterApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CounterApp value={10}/>
+    <CounterApp value={0}/>
   </React.StrictMode> 
 );
